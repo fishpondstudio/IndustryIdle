70dddec9e6b4648859ea32fb71a0fa5180a00cef
insraq <ruoysun@gmail.com>
2022-11-21 21:18:08 +0200

    Update data exporter script

--- exporter/exporter.ts
+++ exporter/exporter.ts
@@ -2,20 +2,22 @@ import { writeFile } from "fs/promises";
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("http://localhost:7456/");
-  let result;
-  while (!result) {
-    result = await page.evaluate("JSON.stringify(window.BLD)");
-    await wait(1000);
-  }
-  await writeFile("../build/web-mobile/static.json", result);
-  await browser.close();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://localhost:7456/");
+    let result;
+    let count = 0;
+    while (!result && ++count <= 10) {
+        result = await page.evaluate("JSON.stringify(window.BLD)");
+        await wait(1000);
+    }
+    console.log(result);
+    await writeFile(__dirname + "/../../build/web-mobile/static.json", result);
+    await browser.close();
 })();
 
 function wait(ms: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
